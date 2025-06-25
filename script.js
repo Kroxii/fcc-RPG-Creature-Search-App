8d8c2213b18ecf69b0bd8da65dd81b106f3a8ed3
kroxii <kilona.soleis@gmail.com>
2025-06-25 15:11:19 +0200

    remove styles.css for style.css

---
+++ script.js
@@ -0,0 +1,5 @@
+const creatureID = document.getElementById('creature-id');
+const creatureName = document.getElementById('creature-name');
+const specialName = document.getElementById('special-name');
+const specialDescription = document.getElementById('special-description');
+const types = document.getElementById('types');
