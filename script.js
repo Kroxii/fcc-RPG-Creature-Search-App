46166a81927dd47019e6b8473c885f96bdaea023
kroxii <kilona.soleis@gmail.com>
2025-06-25 15:42:53 +0200

    media css et début scripting js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const creatureName = document.getElementById('creature-name');
 const specialName = document.getElementById('special-name');
 const specialDescription = document.getElementById('special-description');
 const types = document.getElementById('types');
+const height = document.getElementById('height');
+const weight = document.getElementById('weight');
+const hp = document.getElementById('hp');
+const attack = document.getElementById('attack');
+const defense = document.getElementById('defense');
+const specialAttack = document.getElementById('special-attack');
+const specialDefense = document.getElementById('special-defense');
+const speed = document.getElementById('speed');
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const music = document.getElementById('background-music');
+
